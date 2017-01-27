5fc34dc8480b67aeac45d59c14f1f7a7ff32b406
Beau Jackson <beau.html@gmail.com>
2017-01-27 00:57:25 -0700

    adds review or scores in index.html

--- app wip1/controller.js
+++ app wip1/controller.js
@@ -2,39 +2,53 @@ angular.module("quizApp")
   .controller("myCtrl", function($scope, myService) {
     $scope.item = {} //to iterate over in ng-repeat
     $scope.counter = 1; //to increment on click
+    $scope.userResponses = {
+      total: {
+        correct: 0,
+        incorrect: 0
+      },
+      trivia: {
+        correct: 0,
+        incorrect: 0,
+      }
+    }; //object to contain accumulated answers
 
     var promise = myService.getList1();
     promise.then(function(data) {
-      console.log('data.data[x].title: ' + data.data[1].title);
       $scope.item = data.data;
-      console.log($scope.item);
+      // console.log($scope.item);
     })
 
-    $scope.userResponseModel = {
-      correct: 0,
-      incorrect: 0,
-      triviaCorrect: 0,
-      triviaIncorrect: 0
-    }; //object to contain accumulated answers
-    //
-    $scope.onSubmit = function(entry) {
-      //
-      //   console.log("entry is: " + entry);
-      $scope.counter++;
-      //   console.log('counter now is: ' + $scope.counter);
-      //   //   console.log("Entry: " + $scope.item.answer);
-      //   //
-      //   //
-      //   //   if (entry === $scope.item.answer) {
-      //   //     console.log("nice!");
-      //   //     $scope.userResponseModel.correct += 1;
-      //   //   } else if (entry === "undefined" || entry !== $scope.item.answer) {
-      //   //     console.log("You suck!");
-      //   //     $scope.userResponseModel.incorrect += 1;
-      //   //   }
-      //   //   console.log("Wrong: " + $scope.userResponseModel.incorrect + " Right: " +
-      //   //     $scope.userResponseModel.correct);
-      //   //
-      //   //
+
+    // console.log("userResponses:");
+    // console.dir($scope.userResponses.total.correct);
+
+    $scope.responseSet = function(ans) {
+      console.log(ans);
+      $scope.ans = ans;
     }
-  })
+
+    $scope.onSubmit = function(entry) {
+      let corr = $scope.item[$scope.counter].answer;
+      let ans = $scope.ans;
+      $scope.counter++;
+      if (corr === ans) {
+        $scope.userResponses.total.correct++;
+        $scope.userResponses.trivia.correct++;
+        console.log("Right!");
+      } else if (corr !== ans) {
+        $scope.userResponses.total.incorrect++;
+        $scope.userResponses.trivia.incorrect++;
+        console.log("Wrong!");
+      }
+      // $scope.scores();
+      console.log("$.uR.total.corr: " + $scope.userResponses.total.correct);
+    }
+
+
+    $scope.scores = function() {
+      $scope.summary = $scope.userResponses.total;
+      $scope.sumTrivia = $scope.userResponses.trivia;
+    }
+    $scope.scores();
+  }) //end of controller
